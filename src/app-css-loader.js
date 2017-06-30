e387c37e17598a763ccf22a2f2b478b133b3d3ea
Nick Beers <nicholas.beers@gmail.com>
2017-06-30 11:25:35 -0400

    Correctly including package to pull fonts in and shrink the main.js

---
+++ src/app-css-loader.js
@@ -0,0 +1,7 @@
+
+// Material Icons and Fonts
+import 'material-design-icons/iconfont/material-icons.css'
+
+// Materialize Library
+import 'materialize-css/dist/css/materialize.css'
+import 'materialize-css/dist/js/materialize.js'
